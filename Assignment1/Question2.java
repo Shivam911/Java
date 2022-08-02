8e65096cdbeda2657964e5be12803ae6eae3761a
Shivam911 <44399859+Shivam911@users.noreply.github.com>
2022-08-02 21:47:04 +0530

    Add files via upload

---
+++ Assignment1/Question2.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print(i+1);
+			}
+			System.out.println();
+		}
+	}
+}
